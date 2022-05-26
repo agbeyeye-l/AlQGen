2994c602aeb3a500c9dfabada8d41dca6e8ceedf
admin <agbeyeyeledi@gmail.com>
2022-05-26 08:41:53 +0200

    refactoring

--- generator_modules/__init__.py
+++ generator_modules/__init__.py
@@ -1,6 +1,5 @@
-# Constructor for questgen
 from __future__ import absolute_import
-from generator_modules.encoding import encoding
-from generator_modules.mcq import mcq__
 from generator_modules.mcq import mcq
-from generator_modules.main import QGen, BoolQGen, AnswerPredictor
+from generator_modules.openq import openq
+from generator_modules.boolq import boolq
+
