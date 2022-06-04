a8c00244a2bbf00d70fa40010f75b6d4c378f83d
admin <agbeyeyeledi@gmail.com>
2022-06-04 18:16:29 +0200

    added distractors module to generator package

--- generator_modules/__init__.py
+++ generator_modules/__init__.py
@@ -2,4 +2,5 @@ from __future__ import absolute_import
 from generator_modules.mcq import mcq
 from generator_modules.openq import openq
 from generator_modules.boolq import boolq
+from generator_modules import distractors
 
